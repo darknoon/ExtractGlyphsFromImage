2112fe13b59ab154ba5dd4ffc8e63729bc1f49a9
Andrew Pouliot <andrew@darknoon.com>
2022-06-04 02:49:24 -0400

    Initial Commit

---
+++ Shared/ExtractGlyphsFromImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExtractGlyphsFromImageApp.swift
+//  Shared
+//
+//  Created by Andrew Pouliot on 6/4/22.
+//
+
+import SwiftUI
+
+@main
+struct ExtractGlyphsFromImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
